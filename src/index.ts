613252c4bedf3a1e64dbf9d5284783029306394a
Danny Vernovsky <dvernovsky@interwise.com>
2018-12-13 23:18:51 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Import the interface to Tessel hardware
+import * as tessel from 'tessel';
+
+// Turn one of the LEDs on to start.
+tessel.led[2].on();
+
+// Blink!
+setInterval(() => {
+  tessel.led[2].toggle();
+  tessel.led[3].toggle();
+}, 100);
+
+console.log("I'm blinking! (Press CTRL + C to stop)");
